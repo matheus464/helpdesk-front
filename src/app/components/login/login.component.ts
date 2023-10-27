325a0fb193c676bac1d700f2d210b63adb5a08b5
= <mathmonsil@hotmail.com>
2023-10-27 14:45:50 -0300

    validação formulário login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Credenciais } from 'src/app/models/Credenciais';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  cred: Credenciais = {
+    email: '',
+    senha: ''
+  }
+
+  email = new FormControl(null, Validators.email);
+  senha = new FormControl(null, Validators.minLength(3))
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  validaCampos(): boolean{
+    if(this.email.valid && this.senha.valid){
+      return true
+    }else{
+      return false
+    }
+  }
 }
